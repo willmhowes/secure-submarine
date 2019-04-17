48dae9533ddf95ee118e97faf2984c156ec7765e
Will Howes <willmhowes@gmail.com>
2019-04-17 14:49:52 -0500

    make so SQLquery varies by user clearance_level

--- server/routes/secrets.router.js
+++ server/routes/secrets.router.js
@@ -5,7 +5,8 @@ const { rejectUnauthenticated } = require('../modules/authentication-middleware'
 
 router.get('/', rejectUnauthenticated, (req, res) => {
     console.log('req.user:', req.user);
-    pool.query('SELECT * FROM "secret";')
+    const action = `SELECT * FROM "secret" WHERE "secrecy_level" <= $1;`
+    pool.query(action, [req.user.clearance_level])
         .then(results => res.send(results.rows))
         .catch(error => {
             console.log('Error making SELECT for secrets:', error);
