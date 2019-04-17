a78277a7684b483537c163c8b0bb42b428302809
Will Howes <willmhowes@gmail.com>
2019-04-17 14:43:47 -0500

    make so server requires authentication

--- server/routes/secrets.router.js
+++ server/routes/secrets.router.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
+const { rejectUnauthenticated } = require('../modules/authentication-middleware');
 
-router.get('/', (req, res) => {
+router.get('/', rejectUnauthenticated, (req, res) => {
     console.log('req.user:', req.user);
     pool.query('SELECT * FROM "secret";')
         .then(results => res.send(results.rows))
